12a479eedea03fec0217963bdfcc6f1ec9b9f694
Sidinei Silva <sidinei.silva02@gmail.com>
2021-05-28 16:29:15 -0300

    Criando estilos de texto

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  Color get background;
+  Color get backgroundSecondary;
+  Color get backgroundPrimary;
+  Color get border;
+  Color get textButtonPrimary;
 }
 
 class AppColorsDefault implements AppColors {
   @override
-  Color get background => Color(0xFF40B38C);
+  Color get backgroundPrimary => Color(0xFFFFFFFF);
+
+  @override
+  Color get backgroundSecondary => Color(0xFF40B38C);
+
+  @override
+  Color get border => Color(0xFFDCE0E5);
+
+  @override
+  Color get textButtonPrimary => Color(0xFF666666);
 }
