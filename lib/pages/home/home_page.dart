8c06768b8c9267cb95faa820db64002ccadde026
Sidinei Silva <sidinei.silva02@gmail.com>
2021-11-28 11:48:14 -0300

    Incluindo sliver bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:split_it/pages/home/home_controller.dart';
 import 'package:split_it/pages/home/home_state.dart';
 import 'package:split_it/pages/home/widgets/app_bar/app_bar_widget.dart';
 import 'package:split_it/pages/home/widgets/event_tile/event_tile_widget.dart';
+import 'package:split_it/pages/home/widgets/sliver_app_bar_custom/sliver_app_bar_custom_widget.dart';
 import 'package:split_it/pages/login/models/user_model.dart';
 import 'package:split_it/shared/models/event_model.dart';
 import 'package:split_it/theme/app_theme.dart';
+
+import 'widgets/app_bar/widgets/balance_app_bar/balance_app_bar.dart';
+import 'widgets/app_bar/widgets/button_add/button_add_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -35,43 +40,51 @@ class _HomePageState extends State<HomePage> {
           Navigator.pushNamed(context, "/create_split");
         },
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Observer(builder: (context) {
-              if (homeController.homeState is HomeStateLoading) {
-                return Column(
-                  children: List.generate(
-                    4,
-                    (index) => EventTileWidget(
-                      isLoading: true,
-                      data: EventModel(
-                        date: DateTime.now(),
-                        money: 0,
-                        people: 0,
-                        title: '',
-                      ),
-                    ),
-                  ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBarCustom(),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Observer(
+                  builder: (context) {
+                    if (homeController.homeState is HomeStateLoading) {
+                      return Column(
+                        children: List.generate(
+                          4,
+                          (index) => EventTileWidget(
+                            isLoading: true,
+                            data: EventModel(
+                              date: DateTime.now(),
+                              money: 0,
+                              people: 0,
+                              title: '',
+                            ),
+                          ),
+                        ),
+                      );
+                    } else if (homeController.homeState is HomeStateSuccess) {
+                      return Column(
+                        children: (homeController.homeState as HomeStateSuccess)
+                            .events
+                            .map(
+                              (event) => EventTileWidget(data: event),
+                            )
+                            .toList(),
+                      );
+                    } else if (homeController.homeState is HomeStateFailure) {
+                      return Text((homeController.homeState as HomeStateFailure)
+                          .message);
+                    } else {
+                      return Container();
+                    }
+                  },
                 );
-              } else if (homeController.homeState is HomeStateSuccess) {
-                return Column(
-                  children: (homeController.homeState as HomeStateSuccess)
-                      .events
-                      .map(
-                        (event) => EventTileWidget(data: event),
-                      )
-                      .toList(),
-                );
-              } else if (homeController.homeState is HomeStateFailure) {
-                return Text(
-                    (homeController.homeState as HomeStateFailure).message);
-              } else {
-                return Container();
-              }
-            })
-          ],
-        ),
+              },
+              childCount: 20,
+            ),
+          )
+        ],
       ),
     );
   }
